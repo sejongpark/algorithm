5ee79ea27b659f7779184a6e186569f20b49c170
박세종 <117425885+sejongpark@users.noreply.github.com>
2024-10-09 22:53:43 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -0,0 +1,9 @@
+function solution(s) {
+
+    s = s.toLowerCase();
+    
+    const pCount = s.split('p').length - 1;
+    const yCount = s.split('y').length - 1;
+
+    return pCount === yCount;
+}
